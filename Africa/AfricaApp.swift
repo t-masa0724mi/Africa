7ec49fb755290c0e81f613950639e125270ce94e
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-09 14:51:35 +0900

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by 飯塚政美 on 2023/06/09.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
