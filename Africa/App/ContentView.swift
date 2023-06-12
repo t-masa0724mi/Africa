0cabc02fde2a2d3fca69380cc12cd6ab44532b60
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-12 16:06:09 +0900

    ContentViewの修正

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -2,13 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Conetent")
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }
+            .navigationBarTitle("Africa", displayMode: .large)
         }
-        .padding()
     }
 }
 
