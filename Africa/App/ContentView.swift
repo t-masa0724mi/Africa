1f5a6f3376608d524e6abdc43f174bbd852c7a1e
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-12 16:25:07 +0900

    リストの追加

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -1,12 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
     var body: some View {
         NavigationView {
             List {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                }
             }
             .navigationBarTitle("Africa", displayMode: .large)
         }
