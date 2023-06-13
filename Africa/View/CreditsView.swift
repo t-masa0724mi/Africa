8fa98d07249599a596e5856843f575f2fd693004
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-13 16:45:59 +0900

    Macでのビルド対応

---
+++ Africa/View/CreditsView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        VStack {
+            Image("compass")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 128, height: 128)
+            
+            Text("""
+    Copyright ©︎Masami IIzuka
+    All right reserved
+    Better Apps ♡ Less Code
+    """)
+            .font(.footnote)
+        .multilineTextAlignment(.center)
+        }
+        .padding()
+        .opacity(0.4)
+    }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
